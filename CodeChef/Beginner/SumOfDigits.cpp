c772c241fa426644605be8745a702e0a656a926e
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-01-13 10:11:35 +0530

    change

---
+++ CodeChef/Beginner/SumOfDigits.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+  int t, n;
+  cin>>t;
+
+  while(t--){
+    cin>>n;
+    int rem = n%10, sum=0;
+    while(rem!=0){
+      sum = rem + sum;
+      rem = rem%10;
+      }
+    }
+
+
+  return 0;}
+
