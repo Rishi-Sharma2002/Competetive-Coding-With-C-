f9ae0c18fb7a107e5d3de430a5beb435222c54a5
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-01-23 21:23:56 +0530

    Create sum_of_arrays.cpp

---
+++ Algorithms/sum_of_arrays.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+  char a[100];
+  cin>>a[i];
+
+  for(int i=0; strlen(a); i++ )
+    for(int )
+  return 0;}
+
